95a311cf6a4753a052f37c17b192a4d64cd95841
Berdia <berdia@Berdias-MacBook-Pro.local>
2025-11-21 11:41:28 +0400

    commit

---
+++ server/utils/email.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer")
+
+const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: process.env.SMPT_USER,
+        pass: process.env.SMTP_PASS
+    }
+})
+
+const sendEmail = async (to, subject, text) => {
+    try {
+        await transporter.sendMail({
+            from: "<h1>CartZen</h1>",
+            to,
+            subject,
+            text
+        })
+    } catch(err) {
+        console.log(`Error has been found ${err}`);
+    }
+}
+
+module.exports = sendEmail;
